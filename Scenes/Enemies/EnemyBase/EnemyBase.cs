8c0d241a2b871fce4f56011695218ed84b27bfb2
Raymon_Rutten <80789205+Zerophreak@users.noreply.github.com>
2026-03-23 16:38:08 +0100

    fall protection enemybase

--- Scenes/Enemies/EnemyBase/EnemyBase.cs
+++ Scenes/Enemies/EnemyBase/EnemyBase.cs
@@ -8,6 +8,7 @@ public partial class EnemyBase : CharacterBody2D
 	[Export] private HitBox _hitBox;
 	
 	[Export] protected float _speed = 30.0f;
+	[Export] protected float _fallenOffY = 200.0f;
 
 	protected float _gravity = 800.0f;
 	// Called when the node enters the scene tree for the first time.
@@ -15,4 +16,16 @@ public partial class EnemyBase : CharacterBody2D
 	{
 	}
 
+	public override void _Process(douyble delta)
+	{
+		FallenOff();
+	}
+
+	private FallenOff()
+	{
+		if(GlobalPosition.Y >_fallenOffY)
+		{
+			CallDeferred(MethodName.QueueFree);
+		}
+	}
 }
