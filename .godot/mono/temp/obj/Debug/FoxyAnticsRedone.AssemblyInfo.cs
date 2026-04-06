15aced1d79e35019dc9d602c92ae572a2417c444
Raymon_Rutten <80789205+Zerophreak@users.noreply.github.com>
2026-04-06 15:43:57 +0200

    Update shooter

--- .godot/mono/temp/obj/Debug/FoxyAnticsRedone.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/FoxyAnticsRedone.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoxyAnticsRedone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3ffeb3b39ce3974ac0d46da324d681782f20f9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f668dc87dd0fd5d33e70c2d20e45f75ac0f7bd0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoxyAnticsRedone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoxyAnticsRedone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
