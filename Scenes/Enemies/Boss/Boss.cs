bd0fc5f6f39eb0d18667c707598d136bb66937d0
Raymon_Rutten <80789205+Zerophreak@users.noreply.github.com>
2026-04-27 15:28:20 +0200

    disconnect trigger

--- Scenes/Enemies/Boss/Boss.cs
+++ Scenes/Enemies/Boss/Boss.cs
@@ -3,13 +3,18 @@ using System;
 
 public partial class Boss : Node2D
 {
+	[Export] private AnimationTree _animationTree;
+	[Export] private Area2D _trigger;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_trigger.AreaEntered += OntriggerAreaEntered;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    private void OntriggerAreaEntered(Area2D area)
+    {
+        _animationTree.Set("parameters/conditions/on_trigger", true);
+		_trigger.AreaEntered -= OntriggerAreaEntered;
+    }
+
 }
