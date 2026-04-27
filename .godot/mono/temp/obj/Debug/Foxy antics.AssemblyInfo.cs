b578c770c34c71cb7b591a599a6523a3d23302ab
Raymon_Rutten <80789205+Zerophreak@users.noreply.github.com>
2026-04-27 14:33:01 +0200

    Create boss scene

--- .godot/mono/temp/obj/Debug/Foxy antics.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Foxy antics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Foxy antics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4a18a38fb095a7100c98dcc80c3a9ea212f9887")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c029e95b66162b3ec1efb3831aaf5477d638526")]
 [assembly: System.Reflection.AssemblyProductAttribute("Foxy antics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Foxy antics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
