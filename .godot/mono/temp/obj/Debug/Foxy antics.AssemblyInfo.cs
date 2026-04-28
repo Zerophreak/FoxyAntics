f36b56e542d4ec5044b0dc23735832e43a50f023
Raymon_Rutten <80789205+Zerophreak@users.noreply.github.com>
2026-04-28 13:14:51 +0200

    changes to files

--- .godot/mono/temp/obj/Debug/Foxy antics.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Foxy antics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Foxy antics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c029e95b66162b3ec1efb3831aaf5477d638526")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f4f6eec74ca35fa2d20180d027d5ffbf637664e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Foxy antics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Foxy antics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Scenes/Enemies/Boss/Boss.cs
+++ Scenes/Enemies/Boss/Boss.cs
@@ -5,14 +5,15 @@ public partial class Boss : Node2D
 {
 	[Export] private AnimationTree _animationTree;
 	[Export] private Area2D _trigger;
-	// Called when the node enters the scene tree for the first time.
+	
 	public override void _Ready()
 	{
 		_trigger.AreaEntered += OntriggerAreaEntered;
 	}
 
     private void OntriggerAreaEntered(Area2D area)
-    {
+    {	
+		//condition
         _animationTree.Set("parameters/conditions/on_trigger", true);
 		_trigger.AreaEntered -= OntriggerAreaEntered;
     }
