1402c95a49e4ff5976e71a263d3efb5fd758de24
Raymon_Rutten <80789205+Zerophreak@users.noreply.github.com>
2026-05-05 13:30:58 +0200

    Editor update

--- .godot/mono/temp/obj/Debug/Foxy antics.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Foxy antics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Foxy antics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a24c400278d007b4b204bf996a95d43a5eb1ee5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d8d4b2037f1f7cef3bcbf9de27f6c28f79d1eae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Foxy antics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Foxy antics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
